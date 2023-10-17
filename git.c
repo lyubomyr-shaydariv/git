6f780f3a10c984247f4f8915d52732a07822fbf4
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2023-10-17 13:50:01 +0300

    WIP no-repo `git push URL :branch-to-delete`

--- git.c
+++ git.c
@@ -596,7 +596,7 @@ static struct cmd_struct commands[] = {
 	{ "prune", cmd_prune, RUN_SETUP },
 	{ "prune-packed", cmd_prune_packed, RUN_SETUP },
 	{ "pull", cmd_pull, RUN_SETUP | NEED_WORK_TREE },
-	{ "push", cmd_push, RUN_SETUP },
+	{ "push", cmd_push, RUN_SETUP }, // TODO?
 	{ "range-diff", cmd_range_diff, RUN_SETUP | USE_PAGER },
 	{ "read-tree", cmd_read_tree, RUN_SETUP },
 	{ "rebase", cmd_rebase, RUN_SETUP | NEED_WORK_TREE },
