a655db3022ad0ce8214aab44fa95f7409ea35553
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2024-05-08 18:51:08 +0300

    WIP: `mkdir -p empty && ./git add . | less -p empty`

--- dir.c
+++ dir.c
@@ -2020,6 +2020,7 @@ static enum path_treatment treat_directory(struct dir_struct *dir,
 
 			return path_none;
 		}
+printf("\t%s:%d\t(EMPTY DIR)\n", __FILE__, __LINE__);
 		return path_recurse;
 	}
 
